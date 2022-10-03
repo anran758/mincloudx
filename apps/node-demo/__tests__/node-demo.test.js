f3ca6fa7a06715ab69845656981e6e1a4b1ff22a
anran758 <anran758@gmail.com>
2022-10-03 20:02:10 +0800

    chore: remove node-demo app
    mincloud can only run in the container of faas in node.js environment, there is no need to generate a pure node-demo

--- apps/node-demo/__tests__/node-demo.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const nodeDemo = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(nodeDemo(), 'Hello from nodeDemo');
-console.info("nodeDemo tests passed");
