ca6cba8bb4dceb338c513e40364212eafe15a59d
anran758 <anran758@gmail.com>
2023-02-06 22:29:12 +0800

    feat(cli): encapsulated basic HTTP request tools

---
+++ packages/cli/src/request/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
+
+import { transformErrorMessage } from './utils';
+
+const jar = new CookieJar();
+
+const instance = wrapper(
+  axios.create({
+    baseURL: 'https://cloud.minapp.com/',
+    jar,
+  })
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    // console.log('axios.interceptors.response --> ', response);
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+
+    // transform response status to error message
+    transformErrorMessage(error);
+
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
---
+++ packages/cli/src/request/utils.ts
@@ -0,0 +1,16 @@
+import type { AxiosError } from 'axios';
+import { ERROR_MESSAGE } from './config';
+
+export function transformErrorMessage(error: AxiosError) {
+  const status = error.response?.status
+
+  // 设置错误信息
+  if (status) {
+    const message = ERROR_MESSAGE[status];
+    if (message) {
+      error.message = message;
+    } 
+  }
+
+  return error;
+}
