5f24c6f94055187633234fcffc294c127b2ddec5
anran758 <anran758@gmail.com>
2023-03-27 20:06:24 +0800

    build: 🔧 add prettier and modify eslint
    add prettier to enforce project constraints and ensure eslint compatibility with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   env: {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
+    'prettier/prettier': 'error', // 启用 prettier 的规则
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     // 关闭对禁止使用非空断言的检查
