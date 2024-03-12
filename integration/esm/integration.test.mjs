d836d12a9a0ee52110a20b8db6339597a23ac795
anran758 <anran758@gmail.com>
2024-03-12 18:24:55 +0800

    chore: unify typescript versions and correct erroneous test cases

--- integration/esm/integration.test.mjs
+++ integration/esm/integration.test.mjs
@@ -1,6 +1,6 @@
-const { createFaas } = require('@mincloudx/faas');
-const test = require('node:test');
-const assert = require('node:assert');
+import { createFaas } from '@mincloudx/faas';
+import test from 'node:test';
+import assert from 'node:assert';
 
 test('@mincloudx/faas should export modules', () => {
   assert(createFaas instanceof Function);
