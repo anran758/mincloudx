8307f1cfc864cc08f148e6cb184faa331c8445d8
anran758 <anran758@gmail.com>
2024-03-27 12:08:09 +0800

    feat(type): support dynamic return via plain param

---
+++ packages/io/src/type.ts
@@ -0,0 +1,82 @@
+import type { IncomingHttpHeaders } from 'http';
+import type { BaaS } from '@mincloudx/types';
+
+export type RecordId = string | number;
+
+export interface MinCloudResponse<T = any> {
+  status: number;
+  statusText: string;
+  headers: IncomingHttpHeaders;
+  data: T;
+}
+
+export interface BasicOperationOptions<Plain extends boolean = true> {
+  plain?: Plain;
+}
+
+export interface TableRecord {
+  id: RecordId;
+  [key: string | number]: any;
+}
+
+export interface OperatorData {
+  [key: string | number]: any;
+}
+
+export interface BatchActionParams {
+  enableTrigger?: boolean;
+  withCount?: boolean;
+  expand?: string | string[];
+}
+
+export type QueryOperationOptions = BasicOperationOptions &
+  Omit<BatchActionParams, 'enableTrigger'> & {
+    select?: string[];
+  };
+
+export type UpdateOperationOptions = BasicOperationOptions &
+  Omit<BatchActionParams, 'expand'> & {
+    data?: Record<string | number, any>;
+    unset?: Record<string | number, any>;
+    incrementBy?: Record<string | number, any>;
+    append?: Record<string | number, any>;
+    uAppend?: Record<string | number, any>;
+    remove?: Record<string | number, any>;
+    patchObject?: Record<string | number, any>;
+  };
+
+export interface DeleteOperation extends Omit<BatchActionParams, 'expand'> {
+  id?: RecordId;
+  query?: BaaS.Query;
+  offset?: number;
+  limit?: number;
+}
+
+export type OperationResponse<
+  Data = any,
+  Plain extends boolean = true,
+> = Plain extends true ? Promise<Data> : Promise<MinCloudResponse<Data>>;
+
+export interface Operation {
+  readonly table: BaaS.TableObject;
+  create: <T extends object = OperatorData, Plain extends boolean = true>(
+    data: T,
+    options?: BasicOperationOptions,
+  ) => OperationResponse<T & TableRecord, Plain>;
+  get: <T extends object = TableRecord, Plain extends boolean = true>(
+    id: RecordId,
+    options?: QueryOperationOptions,
+  ) => OperationResponse<T, Plain>;
+  find: <T extends object = OperatorData, Plain extends boolean = true>(
+    query: BaaS.Query,
+    options?: QueryOperationOptions & { orderBy?: string | string[] },
+  ) => OperationResponse<Array<T>, Plain>;
+  update: <T extends object = OperatorData, Plain extends boolean = true>(
+    id: RecordId,
+    options: UpdateOperationOptions,
+  ) => OperationResponse<T & TableRecord, Plain>;
+  delete: (
+    query: DeleteOperation['id'] | DeleteOperation['query'],
+  ) => Promise<any>;
+  count: (query: BaaS.Query) => Promise<number>;
+}
